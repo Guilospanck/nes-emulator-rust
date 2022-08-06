8306d753c9c895066917df8e84b23a7d4e39d589
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-08-06 18:16:17 +0100

    docs: adds editor config

--- study/adc_sbc.asm
+++ study/adc_sbc.asm
@@ -1,17 +1,17 @@
-LDA #$02 ; Loads accumulator with 0x02
+  LDA #$02 ; Loads accumulator with 0x02
 
-TAX 	 ; Transfers 0x02 to Register X
+  TAX 	 ; Transfers 0x02 to Register X
 
-LDA #$01 ; Loads accumulator with 0x01
+  LDA #$01 ; Loads accumulator with 0x01
 
-STA $03	 ; Sets accumulator address to 0x03
-STX $04  ; Sets Register X addr to 0x04
+  STA $03	 ; Sets accumulator address to 0x03
+  STX $04  ; Sets Register X addr to 0x04
 
-ADC $04  ; Adds accumulator to the value of addr 0x04, which is 0x02
+  ADC $04  ; Adds accumulator to the value of addr 0x04, which is 0x02
 
-TAX 	 ; Transfers this value (0x03) to Register X
+  TAX 	 ; Transfers this value (0x03) to Register X
 
-SEC	     ; for correct next subtraction with SBC
-SBC #$01 ; Subtracts the value 0x01, resulting in 0x02
+  SEC	     ; for correct next subtraction with SBC
+  SBC #$01 ; Subtracts the value 0x01, resulting in 0x02
 
-TAY	     ; Transfers 0x02 to Register Y 
+  TAY	     ; Transfers 0x02 to Register Y 
