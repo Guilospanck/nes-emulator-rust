9545209e81952366c94445ba299b44fb50aec22c
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-09-17 11:07:26 +0100

    Add `TXA`

--- src/cpu.rs
+++ src/cpu.rs
@@ -20,6 +20,21 @@ impl CPU {
     }
   }
 
+  fn update_negative_and_zero_flags(&mut self) {
+    // Set Status Flags
+    if self.accumulator == 0 {
+      self.status = self.status | 0b0000_0010; // set zero flag to 1
+    } else {
+      self.status = self.status & 0b1111_1101; // set zero flag to 0
+    }
+
+    if self.accumulator & 0b1000_0000 != 0 {
+      self.status = self.status | 0b1000_0000; // set negative flag to 1
+    } else {
+      self.status = self.status & 0b0111_1111; // set negative flag to 0
+    }
+  }
+
   pub fn interpret(&mut self, program: Vec<u8>) {
     self.program_counter = 0;
 
@@ -34,18 +49,13 @@ impl CPU {
 
           self.accumulator = param;
 
-          // Set Status Flags
-          if self.accumulator == 0 {
-            self.status = self.status | 0b0000_0010; // set zero flag to 1
-          } else {
-            self.status = self.status & 0b1111_1101; // set zero flag to 0
-          }
+          self.update_negative_and_zero_flags();
 
-          if self.accumulator & 0b1000_0000 != 0 {
-            self.status = self.status | 0b1000_0000; // set negative flag to 1
-          } else {
-            self.status = self.status & 0b0111_1111; // set negative flag to 0
-          }
+        },
+        0xAA => { // TAX
+          self.register_x = self.accumulator.clone();
+
+          self.update_negative_and_zero_flags();
 
         },
         0x00 => { // BRK
--- tests/cpu.rs
+++ tests/cpu.rs
@@ -18,7 +18,7 @@ fn test_should_get_lda_immediate_instruction_and_brk_and_set_status_flags_proper
 fn test_should_get_lda_immediate_instruction_and_brk_and_set_zero_status_flag() {
   // arrange
   let expected_status_flags = 0b0000_0010;
-  let program = vec![0xA9, 0x00, 0x00]; // LDA #$0x00  BRK
+  let program = vec![0xA9, 0x00, 0x00]; // LDA #$0x00 ; BRK
   let mut cpu = CPU::new();
 
   // act
@@ -26,4 +26,21 @@ fn test_should_get_lda_immediate_instruction_and_brk_and_set_zero_status_flag() 
 
   // assert
   assert_eq!(cpu.status, expected_status_flags);
+}
+
+#[test]
+fn test_0xaa_tax_move_acc_to_x(){
+  // arrange
+  let mut cpu = CPU::new();
+  let expected_value = 0x01;
+  cpu.accumulator = expected_value;
+  let program = vec![0xAA, 0x00]; // TAX; BRK
+
+  // act
+  cpu.interpret(program);
+
+  // assert
+  assert_eq!(cpu.status, 0);
+  assert_eq!(cpu.accumulator, expected_value);
+  assert_eq!(cpu.register_x, expected_value);
 }
