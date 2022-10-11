509bd7a3d64d2ff2e4e9bc4d4bad65a1aaada9ac
Guilherme Pereira <guilospanck@protonmail.com>
2022-10-11 11:21:51 +0100

    Change JSR test absolute address to be the same as main

--- tests/cpu_opcodes/jsr.rs
+++ tests/cpu_opcodes/jsr.rs
@@ -5,7 +5,7 @@ fn test_0x20_jsr_add_to_acc_and_then_break() {
   // arrange
   let expected_status_flags = 0b1000_0000;
   let lsb_absolute_address = 0x06u8;
-  let msb_absolute_address = 0x80u8;
+  let msb_absolute_address = 0x06u8;
   let acc_value = 0x99;  
   let sec_acc_value = 0x01;
   let expected_acc_value = 0x9A;
@@ -19,7 +19,7 @@ fn test_0x20_jsr_add_to_acc_and_then_break() {
     0x69,
     sec_acc_value,
     0x60,
-  ]; // LDA #$99; JSR $8007; BRK; ADC #$99; RTS
+  ]; // LDA #$99; JSR $0606; BRK; ADC #$99; RTS
   let mut cpu = CPU::new();
 
   // act
@@ -36,7 +36,7 @@ fn test_0x20_jsr_will_break_without_adding_to_acc() {
   // arrange
   let expected_status_flags = 0b1000_0000;
   let lsb_absolute_address = 0x08u8;
-  let msb_absolute_address = 0x80u8;
+  let msb_absolute_address = 0x06u8;
   let acc_value = 0x99;  
   let expected_acc_value = 0x99;
   let program = vec![
@@ -49,7 +49,7 @@ fn test_0x20_jsr_will_break_without_adding_to_acc() {
     acc_value,
     0x60,
     0x00,
-  ]; // LDA #$99; JSR $8007; ADC #$99; RTS; BRK
+  ]; // LDA #$99; JSR $0608; ADC #$99; RTS; BRK
   let mut cpu = CPU::new();
 
   // act
